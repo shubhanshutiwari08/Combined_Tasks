60f8157f4f3a9c7ae6fa7ed7dc84f894ae18b4c4
shubhanshutiwari08 <tiwarishubhanshu1071@gmail.com>
2023-05-06 12:18:44 +0530

    Left and right section created

--- src/componenets/Payement.js
+++ src/componenets/Payement.js
@@ -6,7 +6,33 @@ export default function Payement() {
     <div className='bg-[#363844] h-screen content-center'>
       <div className="max-w-[1040px] mx-auto  min-h-screen">
         <div className='flex justify-center  text-white'>
-            <div className='bg-black text-white p-5 w-full'>one</div>
+            {/* Left Side */}
+            <div className='bg-black text-white p-5 w-full'>
+                <div className='flex flex-col space-y-5'>
+                    <div className='flex space-x-4 '>
+                        <img className='w-8 h-8 rounded-full' src={user} alt="" />
+                        <h1 className='text-center'>Metafy</h1>
+                    </div>
+                    <div className='flex space-x-3'>
+                        <h1>1</h1>
+                        <h1>Gift Card Details</h1>
+                        <h1>2</h1>
+                        <h1>Payment</h1>
+                    </div>
+                    <div className='flex justify-between'>
+                        <div className='flex space-x-5'>
+                            <img className='w-10 h-10 rounded' src={user} alt="" />
+                            <div>
+                                <h1>Metafy Gift Card</h1>
+                                <h1>$20000</h1>
+                            </div>
+                        </div>
+                        <button className='bg-gray-50/10 p-3 text-lg font-semibold'>Change Details</button>
+                    </div>
+                </div>
+                <hr />
+            </div>
+            {/* Right Side */}
             <div className='bg-[#15141a] text-white p-8 w-[40rem] flex flex-col space-y-5'>
                 <div className='flex space-x-4'>
                     <div className='bg-gray-50/10 p-1 rounded-full'><img className='w-12 h-12 rounded-full' src={user} alt="" /></div>
@@ -17,11 +43,11 @@ export default function Payement() {
                 </div>
                 <hr />
                 <div className='flex flex-col'>
-                    <div className='flex justify-around'>
+                    <div className='flex justify-between'>
                         <h1>Subtotal</h1>
                         <h1>$20000</h1>
                     </div>
-                    <div className='flex justify-around'>
+                    <div className='flex justify-between'>
                         <h1>Metlify Credits</h1>
                         <h1>$15.00</h1>
                     </div>
